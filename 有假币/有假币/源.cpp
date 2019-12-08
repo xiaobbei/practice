b70f4e8a7f166477854090e7f0da5bcd5b88321e
xiaobbei <413351042@qq.com>
2019-12-08 19:21:23 +0800

    三等分查找

---
+++ 有假币/有假币/源.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n && n != 0)
+	{
+		if (n == 1)
+			cout << 0 << endl;
+		else if (n <= 3)
+			cout << 1 << endl;
+		else
+		{
+			int count = 1;
+			while (n>3)
+			{
+				if (n % 3 == 0)
+					n /= 3;
+				else
+					n = n / 3 + 1;
+				count++;
+			}
+			cout << count << endl;
+		}
+	}
+	return 0;
+}
