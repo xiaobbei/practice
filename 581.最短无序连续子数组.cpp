453bf444b3b64b33d5a9b98c416545b3d2fa64c5
xiaobbei <413351042@qq.com>
2019-12-18 15:41:07 +0800

    从前往后找无序最大值的位置， 从后往前找无序最小值的位置

---
+++ 581.最短无序连续子数组.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int findUnsortedSubarry(vector<int>& nums)
+{
+	int len = nums.size();
+	int max = nums[0];
+	int min = nums[len - 1];
+	int l = 0,r = -1;
+	for(int i = 0;i<len;++i)
+	{
+		if(max>nums[i])
+		{
+			r = i;
+		}
+		else
+		{
+			max = nums[i];
+		}
+		if(min<nums[len-i-1])
+		{
+			l = len-i-1;
+		}
+		else
+		{
+			min = nums[len-i-1];
+		}
+	}
+	return r-l+1;
+}
+int main()
+{
+	int a[] = {2,6,4,8,10,9,15};
+	vector<int> nums(a,a+7);
+	cout<< findUnsortedSubarry(nums);
+	return 0;
+}
