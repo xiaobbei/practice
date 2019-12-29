2d4ee1f2a3e00defc55948972ad3cfd4b98ce656
xiaobbei <413351042@qq.com>
2019-12-29 20:58:23 +0800

    二分查找变形

---
+++ 33.搜索旋转排序数组/33.搜索旋转排序数组/源.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int search(vector<int>& nums, int target)
+{
+	int left = 0, right = nums.size() - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (nums[mid] == target)
+			return mid;
+		if (nums[left] > nums[mid])
+		{
+			if (nums[mid] < target && nums[right]>=target)
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		else
+		{
+			if (nums[mid] > target &&nums[left] <= target)
+			{
+				right = mid - 1;
+			}
+			else
+				left = mid + 1;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	vector<int> nums{ 4, 5, 6, 7, 0, 1, 2 };
+	cout << search(nums, 0) << endl;
+	system("pause");
+	return 0;
+}
