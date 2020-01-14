a41a8cc488ce4ba4139a032f8f4382a427f125be
xiaobbei <413351042@qq.com>
2020-01-14 22:37:47 +0800

    注意进位

---
+++ 66.加一.cpp
@@ -0,0 +1,26 @@
+#include<iostrea>
+#include<vector>
+
+using namespace std; 
+
+vector<int> plusOne(vector<int>& digits) {
+    for (int i = (int)digits.size() - 1; i >= 0; i--) {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        }
+        else {
+            digits[i]++;
+            break;
+        }
+    }
+    if (digits[0] == 0) {
+        digits.push_back(0);
+        digits[0] = 1;
+    }
+    return digits;
+}
+
+int main()
+{
+	return 0;
+}
