4a76337ffc1d7877133431fb2afb44bfd0c6dfaa
xiaobbei <413351042@qq.com>
2020-01-16 22:43:28 +0800

    遍历

---
+++ 73矩阵置零.cpp
@@ -0,0 +1,35 @@
+#include<iostream> 
+#include<string>
+#include<vector>
+using namespace std;
+void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if (m == 0) return ;
+        int n = matrix[0].size();
+        bool row0 = false, col0 = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) row0 = true;
+                    if (j == 0) col0 = true;
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) 
+                    matrix[i][j] = 0;
+            }
+        }
+        if (col0)
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
+        if (row0)
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
+    }
+
+int main()
+{
+	
+	return 0;
+}
