a177205b02a3b27daa2be8369ce63189954b1d05
xiaobbei <413351042@qq.com>
2020-02-14 23:47:56 +0800

    队列

---
+++ 二叉树的层次遍历.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std; 
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> res;
+    if(!root) return res;
+    queue<TreeNode*> qu;
+    qu.push(root);
+    while(!qu.empty())
+    {
+        vector<int> tmp;
+        int len=qu.size();
+        for(int i=0;i<len;i++){
+            TreeNode* node=qu.front();
+            qu.pop();
+            tmp.push_back(node->val);
+            if(node->left) qu.push(node->left);
+            if(node->right) qu.push(node->right);
+        }
+    res.push_back(tmp);
+    }
+    return res;
+}
+
+int main()
+{
+	return 0;
+}
