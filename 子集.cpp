e116acbb5ec14c5ef44858006704ecf8e654df0a
xiaobbei <413351042@qq.com>
2020-02-28 22:20:18 +0800

    位运算

---
+++ 子集.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ret;
+    ret.push_back({});
+    int size=nums.size();
+    int subsize=pow(2,size);
+    int hash=1;
+    while(hash<subsize){
+        vector<int> temp;
+        for(int k=0;k<size;k++) {
+            int a=1<<k;
+            if(a&hash) {
+                temp.push_back(nums[k]);
+            }
+        }
+        ret.push_back(temp);
+        hash++;
+    }
+    return ret;
+}
+
+
+int main()
+{
+	return 0;
+ } 
